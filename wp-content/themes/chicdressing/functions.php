32f1a764f63c40e236363f50cf7c9b29a6f1db46
bridgte <audrey.poissonneau@gmail.com>
2024-03-25 10:27:15 +0100

    reprise

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -8,6 +8,15 @@ function chicdressing_enqueue_styles() {
 add_filter( 'big_image_size_threshold', '__return_false' );
 
 
-
+//Fonction pour décharger les polices Google du thème parent
+function dequeue_parent_theme_fonts()
+{
+    wp_dequeue_style('kalam');
+    wp_dequeue_style('opensans');
+    wp_dequeue_style('playfair');
+    wp_enqueue_style('rokkitt');
+}
+add_action('wp_enqueue_scripts',
+'dequeue_parent_theme_fonts', 9999);
 
  
